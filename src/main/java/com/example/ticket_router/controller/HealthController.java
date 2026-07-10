5b84f4457177396fcd5de902466b9adc670991fa
MansiNataraj18 <mansi.nataraj@calfus.com>
2026-07-10 12:03:09 +0530

    initial spring boot set up

---
+++ src/main/java/com/example/ticket_router/controller/HealthController.java
@@ -0,0 +1,12 @@
+package com.example.ticket_router.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+    @GetMapping("/health")
+    public String healthCheck() {
+        return "OK";
+    }
+}
